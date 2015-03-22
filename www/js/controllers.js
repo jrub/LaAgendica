eaa9e1d343b86b3ae4b50f006b06815a37cc51ca
Javier Rubio <hola@javirubio.net>
2015-03-22 09:45:54 +0100

    Sanitize HTML devuelto por el API SPARQL

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngSanitize'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Form data for the login modal
